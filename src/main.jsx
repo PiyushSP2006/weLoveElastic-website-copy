a33926c84bc53d5c98c6d36d87b7b6af4991e04c
Aryan <itsaryanplays@gmail.com>
2026-05-27 14:03:34 +0530

    initial setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import React from "react";
+import { createRoot } from "react-dom/client";
+createRoot(document.getElementById("root")).render(
+    <h1>HI from react and git</h1>
 )

f2fc5c2bb6701db2f1394671d6823d9faa51a2bb
Aryan <itsaryanplays@gmail.com>
2026-05-29 00:21:48 +0530

    plugins added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import IntroSection from "../src/IntroSection"
+import {ScrollTrigger,SplitText} from "gsap/all"
+gsap.registerPlugin(ScrollTrigger,SplitText)
 
 export default function App(props){
     return(

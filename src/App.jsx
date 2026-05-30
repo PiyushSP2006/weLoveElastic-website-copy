12536e360220c015dd154dced02a74e39a3fd06a
Aryan <itsaryanplays@gmail.com>
2026-05-30 19:48:35 +0530

    introsection and animmation done >next navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import IntroSection from "../src/IntroSection"
 import {ScrollTrigger,SplitText} from "gsap/all"
+import gsap from "gsap"
 gsap.registerPlugin(ScrollTrigger,SplitText)
 
 export default function App(props){
--- src/IntroSection.jsx
+++ src/IntroSection.jsx
@@ -1,5 +1,41 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import SplitText from "gsap/SplitText.js"
 export default function IntroSection(props){
+    useGSAP(()=>{
+        const loaderTl=gsap.timeline()
+        let lettersGreeting=new SplitText(".greetings",{
+            type:"chars"
+        })
+        loaderTl.to(".loader",{
+            opacity:0,
+            yPercent:-100,
+            duration:3,
+            delay:1,
+            display:"none",
+            ease:"power3.inOut"
+        }).from(".main-content",{
+            opacity:0,
+            yPercent:100,
+            ease:"power3.inOut"
+        },'-=0.2').from(lettersGreeting.chars,{
+            yPercent:100,
+            stagger:0.1,
+            ease:"power3.out"
+        },"<"
+
+        )
+    })
+
+
     return(
-        <h1>here goes the introsection</h1>
+        <div>
+            <div className="loader">
+                <h1>loading animation</h1>
+            </div>
+            <div className="main-content">
+                <h1 className="greetings">hello</h1>
+            </div>
+        </div>
     )
 }
